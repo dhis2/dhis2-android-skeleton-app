5841afdd68a3d87c5e5e3ca6a54c9ffa38cf48d1
Luis Delgado Romera <luisyng@gmail.com>
2019-06-19 17:07:16 +0200

    Remove wrappers in programs and teis activities

--- app/src/main/java/com/example/android/androidskeletonapp/ui/tracked_entity_instances/TrackedEntityInstancesActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/tracked_entity_instances/TrackedEntityInstancesActivity.java
@@ -11,13 +11,12 @@ import com.example.android.androidskeletonapp.data.service.ActivityStarter;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
 import com.example.android.androidskeletonapp.ui.enrollment_form.EnrollmentFormActivity;
 
+import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceCollectionRepository;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceCreateProjection;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -52,61 +51,52 @@ public class TrackedEntityInstancesActivity extends ListActivity {
         if (isEmpty(selectedProgram))
             findViewById(R.id.enrollmentButton).setVisibility(View.GONE);
 
-        findViewById(R.id.enrollmentButton).setOnClickListener(view -> {
-            compositeDisposable.add(
-                    Single.just(Sdk.d2().programModule().programs.uid(selectedProgram).get())
-                            .map(program -> Sdk.d2().trackedEntityModule().trackedEntityInstances
-                                    .add(
-                                            TrackedEntityInstanceCreateProjection.builder()
-                                                    .organisationUnit(Sdk.d2().organisationUnitModule().organisationUnits.one().get().uid())
-                                                    .trackedEntityType(program.trackedEntityType().uid())
-                                                    .build()
-                                    ))
-                            .map(teiUid -> EnrollmentFormActivity.getFormActivityIntent(
-                                    TrackedEntityInstancesActivity.this,
-                                    teiUid,
-                                    selectedProgram,
-                                    Sdk.d2().organisationUnitModule().organisationUnits.one().get().uid()
-                                    ))
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(
-                                    activityIntent ->
-                                            ActivityStarter.startActivity(TrackedEntityInstancesActivity.this, activityIntent),
-                                    Throwable::printStackTrace
-                            )
-            );
-
-
-        });
+        findViewById(R.id.enrollmentButton).setOnClickListener(view -> compositeDisposable.add(
+                Single.just(Sdk.d2().programModule().programs.uid(selectedProgram).get())
+                        .map(program -> Sdk.d2().trackedEntityModule().trackedEntityInstances
+                                .add(
+                                        TrackedEntityInstanceCreateProjection.builder()
+                                                .organisationUnit(Sdk.d2().organisationUnitModule().organisationUnits.one().get().uid())
+                                                .trackedEntityType(program.trackedEntityType().uid())
+                                                .build()
+                                ))
+                        .map(teiUid -> EnrollmentFormActivity.getFormActivityIntent(
+                                TrackedEntityInstancesActivity.this,
+                                teiUid,
+                                selectedProgram,
+                                Sdk.d2().organisationUnitModule().organisationUnits.one().get().uid()
+                                ))
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                activityIntent ->
+                                        ActivityStarter.startActivity(TrackedEntityInstancesActivity.this, activityIntent),
+                                Throwable::printStackTrace
+                        )
+        ));
     }
 
     private void observeTrackedEntityInstances() {
-        RecyclerView trackedEntityInstancesRecyclerView = findViewById(R.id.tracked_entity_instances_recycler_view);
-        trackedEntityInstancesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         TrackedEntityInstanceAdapter adapter = new TrackedEntityInstanceAdapter();
-        trackedEntityInstancesRecyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(adapter);
 
-        compositeDisposable.add(Single.just(Sdk.d2().trackedEntityModule().trackedEntityInstances
-                .withTrackedEntityAttributeValues())
-                .map(teiRepository -> {
-                    if (!isEmpty(selectedProgram)) {
-                        List<String> programUids = new ArrayList<>();
-                        programUids.add(selectedProgram);
-                        teiRepository = teiRepository.byProgramUids(programUids);
-                    }
-                    return teiRepository.getPaged(20);
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(trackedEntityInstances -> {
-                    trackedEntityInstances.observe(this, trackedEntityInstancePagedList -> {
-                        adapter.submitList(trackedEntityInstancePagedList);
-                        findViewById(R.id.tracked_entity_instances_notificator).setVisibility(
-                                trackedEntityInstancePagedList.isEmpty() ? View.VISIBLE : View.GONE);
-                    });
-                }));
+        getTeiRepository().getPaged(20).observe(this, trackedEntityInstancePagedList -> {
+            adapter.submitList(trackedEntityInstancePagedList);
+            findViewById(R.id.tracked_entity_instances_notificator).setVisibility(
+                    trackedEntityInstancePagedList.isEmpty() ? View.VISIBLE : View.GONE);
+        });
+    }
+
+    private TrackedEntityInstanceCollectionRepository getTeiRepository() {
+        TrackedEntityInstanceCollectionRepository teiRepository =
+                Sdk.d2().trackedEntityModule().trackedEntityInstances.withTrackedEntityAttributeValues();
+        if (!isEmpty(selectedProgram)) {
+            List<String> programUids = new ArrayList<>();
+            programUids.add(selectedProgram);
+            return teiRepository.byProgramUids(programUids);
+        } else {
+            return teiRepository;
+        }
     }
 
     @Override
