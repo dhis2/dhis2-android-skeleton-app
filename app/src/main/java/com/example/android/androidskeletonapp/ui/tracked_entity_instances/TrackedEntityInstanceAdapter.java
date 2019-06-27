eeaa8ce159b3210726a0d7159fc3060c0b205cce
Marcos Campos <marcamsn@gmail.com>
2019-06-27 13:21:11 +0200

    Add state icon for teis

--- app/src/main/java/com/example/android/androidskeletonapp/ui/tracked_entity_instances/TrackedEntityInstanceAdapter.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/tracked_entity_instances/TrackedEntityInstanceAdapter.java
@@ -3,12 +3,14 @@ package com.example.android.androidskeletonapp.ui.tracked_entity_instances;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.android.androidskeletonapp.R;
 import com.example.android.androidskeletonapp.data.service.DateFormatHelper;
 import com.example.android.androidskeletonapp.ui.base.DiffByIdItemCallback;
 import com.example.android.androidskeletonapp.ui.base.ListItemWithSyncHolder;
 
+import org.hisp.dhis.android.core.common.State;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityAttributeValue;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstance;
 
@@ -36,21 +38,34 @@ public class TrackedEntityInstanceAdapter extends PagedListAdapter<TrackedEntity
     public void onBindViewHolder(@NonNull ListItemWithSyncHolder holder, int position) {
         TrackedEntityInstance trackedEntityInstance = getItem(position);
         List<TrackedEntityAttributeValue> values = trackedEntityInstance.trackedEntityAttributeValues();
-        if (values != null) {
-            if (values.size() > 0) {
-                holder.title.setText(trackedEntityInstance.trackedEntityAttributeValues().get(0).value());
-            }
-            if (values.size() > 1) {
-                holder.subtitle1.setText(trackedEntityInstance.trackedEntityAttributeValues().get(1).value());
-            }
-            if (values.size() > 2) {
-                holder.subtitle2.setText(trackedEntityInstance.trackedEntityAttributeValues().get(2).value());
-            }
-        }
+        holder.title.setText(valueAt(values, 0));
+        holder.subtitle1.setText(valueAt(values, 1));
+        holder.subtitle2.setText(valueAt(values, 2));
         holder.rightText.setText(DateFormatHelper.formatDate(trackedEntityInstance.created()));
         holder.icon.setImageResource(R.drawable.ic_person_black_24dp);
         setBackgroundColor(R.color.colorAccentDark, holder.icon);
-        holder.syncIcon.setImageResource(R.drawable.ic_sync);
-        setBackgroundColor(R.color.colorAccent, holder.syncIcon);
+        setState(trackedEntityInstance.state(), holder.syncIcon);
+    }
+
+    private void setState(State state, ImageView syncIcon) {
+        if (state == null) {
+            syncIcon.setVisibility(View.GONE);
+        } else {
+            syncIcon.setVisibility(View.VISIBLE);
+             if (state.equals(State.TO_UPDATE) || state.equals(State.TO_POST)|| state.equals(State.TO_DELETE)) {
+                syncIcon.setImageResource(R.drawable.ic_not_sync);
+                setBackgroundColor(R.color.colorAccentAlt, syncIcon);
+            } else if (state.equals(State.ERROR) || state.equals(State.WARNING)) {
+                syncIcon.setImageResource(R.drawable.ic_sync_problem);
+                setBackgroundColor(R.color.colorWarn, syncIcon);
+            } else {
+                syncIcon.setImageResource(R.drawable.ic_sync);
+                setBackgroundColor(R.color.colorAccent, syncIcon);
+            }
+        }
+    }
+
+    private String valueAt(List<TrackedEntityAttributeValue> values, int index) {
+        return values != null && values.size() > index ? values.get(index).value() : null;
     }
 }
