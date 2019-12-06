2f9cc760a1a26e1f26768b8e8793a663c0f3758f
Marcos Campos <marcamsn@gmail.com>
2019-12-06 11:51:49 +0530

    ex09a

--- app/src/main/java/com/example/android/androidskeletonapp/ui/code_executor/CodeExecutorActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/code_executor/CodeExecutorActivity.java
@@ -11,13 +11,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
+import com.example.android.androidskeletonapp.data.utils.Exercise;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import org.hisp.dhis.android.core.organisationunit.OrganisationUnit;
+import org.hisp.dhis.android.core.program.Program;
+import org.hisp.dhis.android.core.program.ProgramTrackedEntityAttribute;
+import org.hisp.dhis.android.core.trackedentity.TrackedEntityAttribute;
+import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceCreateProjection;
+
+import java.util.List;
 
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+
+import static com.example.android.androidskeletonapp.data.service.AttributeHelper.MALARIA_CASE_TET_UID;
 
 public class CodeExecutorActivity extends AppCompatActivity {
 
@@ -65,7 +77,9 @@ public class CodeExecutorActivity extends AppCompatActivity {
                                 view.setEnabled(Boolean.TRUE);
                                 view.setVisibility(View.VISIBLE);
                             },
-                            Throwable::printStackTrace);
+                            error -> {
+                                error.printStackTrace();
+                            });
         });
     }
 
@@ -88,8 +102,26 @@ public class CodeExecutorActivity extends AppCompatActivity {
         }
     }
 
+    @Exercise(
+            exerciseNumber = "ex09a",
+            version = 1,
+            title = "Create a tracked entity instance and set its program tracked entity attribute values.",
+            tips = "Use the organisation unit module to get one organisation unit." +
+                    "Create a TEI using the tracked entity module." +
+                    "Use the program module to get the malaria case program filtering by " +
+                    "byTrackedEntityTypeUid 'MALARIA_CASE_TET_UID'." +
+                    "Use the program module to get the program tracked entity attributes of the program." +
+                    "For each program tracked entity attribute set a tracked entity attribute value." +
+                    "Check on the tracked entity instance list that the TEI has been created.",
+            solutionBranch = "sol09a"
+    )
+
     private Single<String> executeCode() {
-        return Single.just("Execution done!");
+        return Single.defer(() -> {
+            // TODO Create the tracked entity instance here.
+
+            return Single.just("Tracked entity instance created!");
+        });
     }
 
 }
