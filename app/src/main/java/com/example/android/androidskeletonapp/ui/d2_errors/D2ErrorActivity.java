4f92bbd3b1e70c3886cc7373e4cd0df1bcdb6502
Marcos Campos <marcamsn@gmail.com>
2019-12-06 14:52:11 +0530

    ex11a

--- app/src/main/java/com/example/android/androidskeletonapp/ui/d2_errors/D2ErrorActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/d2_errors/D2ErrorActivity.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.paging.PagedList;
 
 import com.example.android.androidskeletonapp.R;
-import com.example.android.androidskeletonapp.data.Sdk;
+import com.example.android.androidskeletonapp.data.utils.Exercise;
 import com.example.android.androidskeletonapp.ui.base.ListActivity;
 
 import org.hisp.dhis.android.core.maintenance.D2Error;
@@ -31,13 +32,26 @@ public class D2ErrorActivity extends ListActivity {
         D2ErrorAdapter adapter = new D2ErrorAdapter();
         recyclerView.setAdapter(adapter);
 
-        LiveData<PagedList<D2Error>> liveData = Sdk.d2().maintenanceModule().d2Errors()
-                .getPaged(20);
+        LiveData<PagedList<D2Error>> liveData = d2ErrorsLiveData();
 
         liveData.observe(this, d2ErrorPagedList -> {
             adapter.submitList(d2ErrorPagedList);
             findViewById(R.id.d2ErrorsNotificator).setVisibility(
                     d2ErrorPagedList.isEmpty() ? View.VISIBLE : View.GONE);
         });
+    }
+
+    @Exercise(
+            exerciseNumber = "ex11a",
+            version = 1,
+            title = "Show the D2Errors list.",
+            tips = "Use the maintenance module to return the d2 errors paged with a page size of 20." +
+                    "Filter by the API_RESPONSE_PROCESS_ERROR D2ErrorCode." +
+                    "Set fly mode and try to sync the metadata." +
+                    "Go to the D2Error list and check that the error has been listed.",
+            solutionBranch = "sol11a"
+    )
+    private LiveData<PagedList<D2Error>> d2ErrorsLiveData() {
+        return new MutableLiveData<>();
     }
 }
