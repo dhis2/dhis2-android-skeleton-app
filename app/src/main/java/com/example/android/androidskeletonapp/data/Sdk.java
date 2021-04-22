54f1dc8b3163212e2c5d0a4314963f942af42e99
Victor Garcia <vgarciabnz@gmail.com>
2021-04-22 13:57:21 +0200

    [MASTER-SDK-1.4.0] Update sdk submodule ref

--- app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
+++ app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
@@ -2,16 +2,7 @@ package com.example.android.androidskeletonapp.data;
 
 import android.content.Context;
 
-import com.example.android.androidskeletonapp.BuildConfig;
-import com.facebook.flipper.android.AndroidFlipperClient;
-import com.facebook.flipper.android.utils.FlipperUtils;
-import com.facebook.flipper.core.FlipperClient;
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin;
-import com.facebook.flipper.plugins.inspector.DescriptorMapping;
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
-import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor;
-import com.facebook.flipper.plugins.network.NetworkFlipperPlugin;
-import com.facebook.soloader.SoLoader;
+import com.example.android.androidskeletonapp.data.service.FlipperManager;
 
 import org.hisp.dhis.android.core.D2;
 import org.hisp.dhis.android.core.D2Configuration;
@@ -30,7 +21,7 @@ public class Sdk {
 
     public static D2Configuration getD2Configuration(Context context) {
         // This will be null if not debug mode to make sure your data is safe
-        Interceptor flipperInterceptor = getFlipperInterceptor(context.getApplicationContext());
+        Interceptor flipperInterceptor = FlipperManager.setUp(context.getApplicationContext());
 
         List<Interceptor> networkInterceptors = new ArrayList<>();
         if (flipperInterceptor != null) {
@@ -46,20 +37,5 @@ public class Sdk {
                 .networkInterceptors(networkInterceptors)
                 .context(context)
                 .build();
-    }
-
-    private static Interceptor getFlipperInterceptor(Context context) {
-        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(context)) {
-            NetworkFlipperPlugin networkPluggin = new NetworkFlipperPlugin();
-            SoLoader.init(context, false);
-            FlipperClient client = AndroidFlipperClient.getInstance(context);
-            client.addPlugin(networkPluggin);
-            client.addPlugin(new DatabasesFlipperPlugin(context));
-            client.addPlugin(new InspectorFlipperPlugin(context, DescriptorMapping.withDefaults()));
-            client.start();
-            return new FlipperOkhttpInterceptor(networkPluggin);
-        } else {
-            return null;
-        }
     }
 }
