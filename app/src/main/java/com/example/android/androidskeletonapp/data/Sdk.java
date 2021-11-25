e514f55070618ce70332fc49676e1899f6b4d661
andresmr <andres@andresmr.com>
2021-11-25 23:11:39 +0100

    Update dependencies

--- app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
+++ app/src/main/java/com/example/android/androidskeletonapp/data/Sdk.java
@@ -2,16 +2,9 @@ package com.example.android.androidskeletonapp.data;
 
 import android.content.Context;
 
-import com.example.android.androidskeletonapp.data.service.FlipperManager;
-
 import org.hisp.dhis.android.core.D2;
 import org.hisp.dhis.android.core.D2Configuration;
 import org.hisp.dhis.android.core.D2Manager;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.Interceptor;
 
 public class Sdk {
 
@@ -20,21 +13,12 @@ public class Sdk {
     }
 
     public static D2Configuration getD2Configuration(Context context) {
-        // This will be null if not debug mode to make sure your data is safe
-        Interceptor flipperInterceptor = FlipperManager.setUp(context.getApplicationContext());
-
-        List<Interceptor> networkInterceptors = new ArrayList<>();
-        if (flipperInterceptor != null) {
-            networkInterceptors.add(flipperInterceptor);
-        }
-
         return D2Configuration.builder()
                 .appName("skeleton_App")
                 .appVersion("0.0.1")
                 .readTimeoutInSeconds(30)
                 .connectTimeoutInSeconds(30)
                 .writeTimeoutInSeconds(30)
-                .networkInterceptors(networkInterceptors)
                 .context(context)
                 .build();
     }
