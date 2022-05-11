2b0dd62d047cda18cb851d7517d8e5dfc02d5dd0
Gift <nnkogift@hotmail.com>
2022-05-11 11:20:49 +0300

    :bug: fix(code executor): Fixes
    -  Implemented org unit get to get for specific dataset

--- app/src/main/java/com/example/android/androidskeletonapp/ui/code_executor/CodeExecutorActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/code_executor/CodeExecutorActivity.java
@@ -11,14 +11,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 import com.example.android.androidskeletonapp.data.utils.Exercise;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import com.google.common.collect.Lists;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import org.hisp.dhis.android.core.category.CategoryOptionCollectionRepository;
+import org.hisp.dhis.android.core.category.CategoryOptionCombo;
+import org.hisp.dhis.android.core.category.CategoryOptionComboCollectionRepository;
+import org.hisp.dhis.android.core.dataelement.DataElement;
+import org.hisp.dhis.android.core.dataset.DataSet;
+import org.hisp.dhis.android.core.dataset.DataSetCompleteRegistration;
+import org.hisp.dhis.android.core.dataset.DataSetElement;
+import org.hisp.dhis.android.core.datavalue.DataValueObjectRepository;
+import org.hisp.dhis.android.core.maintenance.D2Error;
+import org.hisp.dhis.android.core.organisationunit.OrganisationUnit;
+import org.hisp.dhis.android.core.period.Period;
+
+import java.util.List;
+import java.util.Objects;
 
 public class CodeExecutorActivity extends AppCompatActivity {
 
@@ -90,22 +106,58 @@ public class CodeExecutorActivity extends AppCompatActivity {
     }
 
     @Exercise(
-        exerciseNumber = "ex05a-aggregatedDataCreation",
-        title = "Set all the data values of one data set instance",
-        tips = "Use the data set module to get the data set Population: aLpVgfXiz0f with its data set elements" +
-                "Use the period module to get a period with the same period type that the data set." +
-                "Use the organisation unit module to get one organisation unit." +
-                "Use the category module to get the attribute option combo (with the " +
-                "categoryOptionComboRepository) related to the data set." +
-                "Set a random value for each data element (Using nested loops)." +
-                "Note that you can get the category option combo from the data element in the loop."
+            exerciseNumber = "ex05a-aggregatedDataCreation",
+            title = "Set all the data values of one data set instance",
+            tips = "Use the data set module to get the data set Population: aLpVgfXiz0f with its data set elements" +
+                    "Use the period module to get a period with the same period type that the data set." +
+                    "Use the organisation unit module to get one organisation unit." +
+                    "Use the category module to get the attribute option combo (with the " +
+                    "categoryOptionComboRepository) related to the data set." +
+                    "Set a random value for each data element (Using nested loops)." +
+                    "Note that you can get the category option combo from the data element in the loop."
     )
     private Single<String> executeCode() {
         return Single.defer(() -> {
-            // TODO resolve the exercise here.
             String DATA_SET_UID = "aLpVgfXiz0f";
+            DataSet dataSet = Sdk.d2().dataSetModule().dataSets().withDataSetElements().uid(DATA_SET_UID).blockingGet();
+            Single<List<Period>> periods = Sdk.d2().periodModule().periodHelper().getPeriodsForDataSet(DATA_SET_UID);
+            Period period = periods.blockingGet().get(0);
+            OrganisationUnit organisationUnit = Sdk.d2()
+                    .organisationUnitModule()
+                    .organisationUnits()
+                    .byDataSetUids(Lists.newArrayList(dataSet.uid()))
+                    .one().blockingGet();
 
+            CategoryOptionCombo dataSetOptionCombo = Sdk.d2()
+                    .categoryModule()
+                    .categoryOptionCombos()
+                    .byCategoryComboUid()
+                    .eq(dataSet.categoryCombo().uid())
+                    .one().blockingGet();
 
+            for (DataSetElement dataSetElement : Objects.requireNonNull(dataSet.dataSetElements())) {
+                DataElement dataElement = Sdk.d2().dataElementModule().dataElements().uid(dataSetElement.dataElement().uid()).blockingGet();
+                String categoryComboUid = dataElement.categoryComboUid();
+                List<CategoryOptionCombo> categoryOptionCombos = Sdk.d2()
+                        .categoryModule()
+                        .categoryOptionCombos()
+                        .byCategoryComboUid()
+                        .eq(categoryComboUid)
+                        .blockingGet();
+
+                for (CategoryOptionCombo optionCombo : categoryOptionCombos) {
+                    DataValueObjectRepository dataValueObjectRepository = Sdk.d2()
+                            .dataValueModule()
+                            .dataValues()
+                            .value(period.periodId(), organisationUnit.uid(), dataElement.uid(), optionCombo.uid(), dataSetOptionCombo.uid());
+
+                    try {
+                        dataValueObjectRepository.blockingSet(String.valueOf((Math.random() * 20)));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
             return Single.just("Execution done!");
         });
     }
